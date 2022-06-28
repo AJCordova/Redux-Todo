dcee3b710427ae1a615cc5d4456315c62a4e2582
AJCordova <archonist252@gmail.com>
2022-06-28 14:08:17 +0800

    Create UI for TODOViewController.

---
+++ Redux-Todo/Scenes/Todo/TodoViewController.swift
@@ -0,0 +1,125 @@
+//
+//  TodoViewController.swift
+//  Redux-Todo
+//
+//  Created by COLLABERA on 6/26/22.
+//
+
+import Foundation
+import UIKit
+
+class TodoViewController: UIViewController {
+    
+    lazy var todoHeaderLabel: UILabel = UILabel()
+    lazy var changeUserButton: UIButton = UIButton()
+    lazy var addTodoButton: UIButton = UIButton()
+    lazy var todoTableView: UITableView = UITableView()
+    
+    // private var viewModel: TodoViewModelTypes
+    private let todoTableCellID = "TodoTableViewCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        view.backgroundColor = .systemBackground
+        
+        setupViews()
+    }
+}
+
+extension TodoViewController {
+    func setupViews() {
+        setupTodoHeaderLabel()
+        setupChangeUserButton()
+        setupAddTodoButton()
+        setupTodoTableView()
+    }
+    
+    func setupTodoHeaderLabel() {
+        todoHeaderLabel.text = "User's TODOs"
+        todoHeaderLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        view.addSubview(todoHeaderLabel)
+        
+        todoHeaderLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(80)
+            make.leading.equalToSuperview().offset(20)
+            make.height.equalTo(20)
+        }
+    }
+    
+    func setupChangeUserButton() {
+        let imageConfig = UIImage.SymbolConfiguration(font: .boldSystemFont(ofSize: 30),
+                                                      scale: .medium)
+        let changeUserButtonImage = UIImage(systemName: "person.crop.circle",
+                                            withConfiguration: imageConfig)
+        changeUserButton.setImage(changeUserButtonImage, for: .normal)
+        changeUserButton.tintColor = .systemGreen
+        changeUserButton.isEnabled = true
+        view.addSubview(changeUserButton)
+        
+        changeUserButton.snp.makeConstraints { make in
+            make.height.equalTo(30)
+            make.width.equalTo(30)
+            make.top.equalToSuperview().offset(75)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+    }
+    
+    func setupTodoTableView() {
+        todoTableView.register(UINib.init(nibName: "TodoTableViewCell", bundle: nil), forCellReuseIdentifier: todoTableCellID)
+        view.addSubview(todoTableView)
+        
+        todoTableView.snp.makeConstraints { make in
+            make.top.equalTo(todoHeaderLabel.snp.bottom).offset(50)
+            make.left.right.equalToSuperview().offset(20)
+            make.height.equalTo(540)
+        }
+    }
+    
+    func setupAddTodoButton() {
+        addTodoButton.backgroundColor = .systemGreen
+        addTodoButton.setTitle("Add Todo", for: .normal)
+        addTodoButton.layer.cornerRadius = 9.0
+        view.addSubview(addTodoButton)
+        
+        addTodoButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-50)
+            make.left.right.equalToSuperview().inset(50)
+        }
+    }
+}
+
+extension TodoViewController: UITableViewDelegate {
+    
+}
+
+//    var encodedJson = ""
+//    var testModel: UserModel?
+//    @IBAction func jsonAction(_ sender: Any) {
+//        do {
+//            let data = try JSONEncoder().encode(SampleData.testUserModel)
+//            if let jsonString = String(data: data, encoding: .utf8) {
+//                print(jsonString)
+//                encodedJson = jsonString
+//            }
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//    }
+//
+//    @IBAction func decodeJson(_ sender: Any) {
+//        let encodedJson = Data(encodedJson.utf8)
+//        do {
+//            let decoded = try JSONDecoder().decode(UserModel.self, from: encodedJson)
+//            testModel = decoded
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//
+//        guard let testModelTasks = testModel?.tasks else { return }
+//
+//        for testModelTask in testModelTasks {
+//            print(testModelTask.title)
+//            print(testModelTask.details)
+//        }
+//    }
